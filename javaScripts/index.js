94062546cc083f8aa6889174d9a3cfe3f4423600
Yasin051197 <yasinjamadar0511@gmail.com>
2022-07-23 16:23:28 +0530

    added file

--- javaScripts/index.js
+++ javaScripts/index.js
@@ -2,5 +2,23 @@ document.querySelector("#home").addEventListener("click",homefun)
 
 function homefun()
 {
-    <a href="http://127.0.0.1:5500/benevolent-boot-3435/index.html" target="_blank"></a>
+    window.location.href="http://127.0.0.1:5500/benevolent-boot-3435/index.html"
+}
+document.querySelector("#Nat").addEventListener("click",Natfun)
+
+function Natfun()
+{
+    window.location.href="http://127.0.0.1:5500/benevolent-boot-3435/Nation.html"
+}
+document.querySelector("#Sou").addEventListener("click",Soufun)
+
+function Soufun()
+{
+    window.location.href="http://127.0.0.1:5500/benevolent-boot-3435/South.html"
+}
+document.querySelector("#Wor").addEventListener("click",Worfun)
+
+function Worfun()
+{
+    window.location.href="http://127.0.0.1:5500/benevolent-boot-3435/World.html"
 }
