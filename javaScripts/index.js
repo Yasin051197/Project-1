f918f0fc7d5eaa01defe67f646d802f0a34acacc
Yasin051197 <yasinjamadar0511@gmail.com>
2022-07-23 12:37:15 +0530

    nation file

---
+++ javaScripts/index.js
@@ -0,0 +1,6 @@
+document.querySelector("#home").addEventListener("click",homefun)
+
+function homefun()
+{
+    <a href="http://127.0.0.1:5500/benevolent-boot-3435/index.html" target="_blank"></a>
+}
